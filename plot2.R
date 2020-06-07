fd9abf4502c92a11a1b9e38af82db03e3f1c472e
shuhaodexiaowu <zhangshuhaojs@163.com>
2020-06-07 13:05:38 +0800

    plot2

---
+++ plot2.R
@@ -0,0 +1,7 @@
+data1 <- read.table("F:/coursera/household_power_consumption.txt",header =TRUE,sep =";",na.string ="?")
+data2 <- subset(data1, Date =="1/2/2007" | Date =="2/2/2007")
+data2$DateTime <-paste(data2$Date, data2$Time)
+data2$DateTime <- strptime(data2$DateTime, "%d/%m/%Y %H:%M:%S")
+plot(data2$DateTime,data2$Global_active_power,type ="l",xlab ="n",ylab ="Global Active Power(kilowatts")
+dev.copy(png,file="plot2.png",height=480, width=480)
+dev.off()
